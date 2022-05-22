026f8e869b7f9c57bfc46edce86c26c29e613042
ark1987s <arkadiy1987@yandex.ru>
2022-05-22 15:06:04 +0300

    new dir - Utils

---
+++ ITMO.CSCourse2022.Lab04.Utils/Utils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2022.Lab04.Utils
+{
+    class Utils
+    {
+        public static int Greater(int a, int b)
+        {
+            if (a > b)
+                return a;
+            else
+                return b;
+        }
+    }
+    public class Test
+    {
+        public static void Main()
+        {
+            int x;
+            int y;
+            int greater;
+            Console.WriteLine("Enter first number:");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            y = int.Parse(Console.ReadLine());
+            greater = Utils.Greater(x,y);
+            Console.WriteLine("The greater value is " + greater);
+        }
+    }
+}
