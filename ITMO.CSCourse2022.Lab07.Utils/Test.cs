c0dc4a3d2ccdf6098f94ea510ce919eb905c12ce
ark1987s <arkadiy1987@yandex.ru>
2022-05-27 02:14:21 +0300

    new dir - Lab07.Utils

---
+++ ITMO.CSCourse2022.Lab07.Utils/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2022.Lab07.Utils
+{
+    public class Test
+    {
+        static void Main()
+        {
+            string message;
+            Console.WriteLine("Enter string to reverse:");
+            message = Console.ReadLine();
+            Utils.Reverse(ref message);
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ ITMO.CSCourse2022.Lab07.Utils/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2022.Lab07.Utils
+{
+    class Utils
+    {                        
+        public static void Reverse(ref string s)
+        {            
+            string sRev = "";
+            for (int k = s.Length - 1; k >= 0; k--)
+            sRev = sRev + s[k];
+            s = sRev;
+        }
+    }
+}
