9c9779584176160421899b2dc1e13d0ed79b9885
ark1987s <arkadiy1987@yandex.ru>
2022-05-27 00:32:21 +0300

    new dir - Lab07.Bank

---
+++ ITMO.CSCourse2022.Lab06.CreateAccount/BankAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2022.Lab06.CreateAccount
+{
+    struct BankAccount
+    {
+        public long accNo;
+        public decimal accBal;
+        public AccountType accType;
+    }
+}
---
+++ ITMO.CSCourse2022.Lab07.Bank/AccountType .cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2022.Lab07.Bank
+{
+    enum AccountType
+    {
+        Checking,
+        Deposit
+    }
+
+}
