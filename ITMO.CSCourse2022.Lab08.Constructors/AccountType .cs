45481d0c9bc77d23955c03ed40dd103fdb2ba2f3
ark1987s <arkadiy1987@yandex.ru>
2022-05-28 13:54:36 +0300

    Constructors

---
+++ ITMO.CSCourse2022.Lab08.Constructors/AccountType .cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2022.Lab08.Constructors
+{    enum AccountType
+    {
+        Checking,
+        Deposit
+    }
+}
---
+++ ITMO.CSCourse2022.Lab08.Constructors/BankAccount.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2022.Lab08.Constructors
+{
+    class BankAccount
+    {
+		private long accNo;
+		private decimal accBal;
+		private AccountType accType;
+		private static long nextNumber = 123;
+		public BankAccount()
+		{
+			accNo = NextNumber();
+			accType = AccountType.Checking;
+			accBal = 0;
+		}
+
+		public BankAccount(AccountType aType)
+		{
+			accNo = NextNumber();
+			accType = aType;
+			accBal = 0;
+		}
+
+		public BankAccount(decimal aBal)
+		{
+			accNo = NextNumber();
+			accType = AccountType.Checking;
+			accBal = aBal;
+		}
+
+		public BankAccount(AccountType aType, decimal aBal)
+		{
+			accNo = NextNumber();
+			accType = aType;
+			accBal = aBal;
+		}
+		public long Number()
+		{
+			return accNo;
+		}
+
+		public decimal Balance()
+		{
+			return accBal;
+		}
+
+		public string Type()
+		{
+			return accType.ToString();
+		}
+
+		private static long NextNumber()
+		{
+			return nextNumber++;
+		}
+	}
+
+}
