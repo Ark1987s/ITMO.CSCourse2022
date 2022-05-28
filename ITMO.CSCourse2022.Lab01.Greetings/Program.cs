e5a68722b55c23069e026c01bb89884ee2555fc9
ark1987s <arkadiy1987@yandex.ru>
2022-05-29 00:23:27 +0300

    update

--- ITMO.CSCourse2022.Lab01.Greetings/Program.cs
+++ ITMO.CSCourse2022.Lab01.Greetings/Program.cs
@@ -11,7 +11,6 @@ namespace ITMO.CSCourse2022.Lab01.Greetings
         static void Main(string[] args)
         {
             string myName;
-
             Console.WriteLine("Please enter your name");
             myName = Console.ReadLine();
             Console.WriteLine("Hello, {0}!", myName);
