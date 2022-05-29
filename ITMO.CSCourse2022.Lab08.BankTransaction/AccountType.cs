28b3381a4b04bf4265a0e444129c195e603be84e
ark1987s <arkadiy1987@yandex.ru>
2022-05-30 02:48:24 +0300

    new

---
+++ ITMO.CSCourse2022.Lab08.BankTransaction/AccountType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2022.Lab08.BankTransaction
+{
+    enum AccountType
+    {
+        Checking,
+        Deposit
+    }
+}
---
+++ ITMO.CSCourse2022.Lab08.BankTransaction/BankTransaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ITMO.CSCourse2022.Lab08.BankTransaction
+{
+    public class BankTransaction
+    {
+        private readonly decimal amount;
+        private readonly DateTime when;
+        public decimal Amount()
+        {
+            return amount;
+        }
+        public DateTime When()
+        {
+            return when;
+        }
+        public BankTransaction(decimal tranAmount)
+        {
+            amount = tranAmount;
+            when = DateTime.Now;
+        }
+    }
+}
+     
+    
