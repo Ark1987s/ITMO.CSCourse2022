117988875c34a50488aeeb307d8a93a820f864ec
ark1987s <arkadiy1987@yandex.ru>
2022-05-29 21:14:26 +0300

    new

---
+++ ITMO.CSCourse2022.Lab06.UniqueNumbers/AccountType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2022.Lab06.UniqueNumbers
+{
+    enum AccountType
+    {
+        Checking,
+        Deposit
+    }
+}
---
+++ ITMO.CSCourse2022.Lab06.UniqueNumbers/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2022.Lab06.UniqueNumbers
+{
+    class BankAccount
+    {
+        public void Populate(decimal balance)
+        {
+            accNo = NextNumber();
+            accBal = balance;
+            accType = AccountType.Checking;
+        }
+        public long Number()
+        {
+            return accNo;
+        }
+        public decimal Balance()
+        {
+            return accBal;
+        }
+        public string Type()
+        {
+            return accType.ToString();
+        }
+        private static long NextNumber()
+        {
+            return nextAccNo++;
+        }
+        private long accNo;
+        private decimal accBal;
+        private AccountType accType;
+        private static long nextAccNo=123;
+    }
+}
