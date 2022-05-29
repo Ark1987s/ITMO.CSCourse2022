ef7c2b5f265c31aff37f7cdafaf28ec40686fba4
ark1987s <arkadiy1987@yandex.ru>
2022-05-29 14:28:49 +0300

    update

--- ITMO.CSCourse2022.Lab04.Swap/Swap.cs
+++ ITMO.CSCourse2022.Lab04.Swap/Swap.cs
@@ -20,16 +20,14 @@ namespace ITMO.CSCourse2022.Lab04.Swap
         public static void Main(string[] args)
         {
             int x;
-            int y;
-            int greater;
+            int y;           
             Console.WriteLine("Enter first number:");
             x = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter second number:");
             y = int.Parse(Console.ReadLine());
-            Console.WriteLine("Before swap: " + x + "," + y);
+            Console.WriteLine("Before swap: " + x + ", " + y);
             Utils.Swap(ref x, ref y);
-            Console.WriteLine("After swap: " + x + "," + y);
-
+            Console.WriteLine("After swap: " + x + ", " + y);
         }
     }
 }
