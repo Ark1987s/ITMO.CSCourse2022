7b92068ee92244a4795b2b156e7518b939ffba0d
ark1987s <arkadiy1987@yandex.ru>
2022-05-31 03:04:32 +0300

    new

---
+++ ITMO.CSCourse2022.Lab10.Banking/BankTransaction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Banking
+{
+	internal class BankTransaction
+	{
+		private readonly decimal amount;
+		private readonly DateTime when;
+		public BankTransaction(decimal tranAmount)
+		{
+			amount = tranAmount;
+			when = DateTime.Now;
+		}
+		public decimal Amount()
+		{
+			return amount;
+		}
+		public DateTime When()
+		{
+			return when;
+		}
+	}
+}
